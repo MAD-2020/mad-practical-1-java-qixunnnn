b62334fc7d10530e802aecc2d7f50ecfe1a883f3
qixunnnn <64120978+qixunnnn@users.noreply.github.com>
2020-04-23 01:10:09 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -9,8 +9,9 @@ public class Question3
      * 
      * Hint: in.nextInt() to read integer
      */
-     
-    Scanner in = new Scanner(System.in);
-    
+      Scanner in = new Scanner(System.in);
+      int num = in.nextInt();
+
+      System.out.println(num*num);
   }
 }
