1e52d5912c8e2836be539508ce2fbf77168b0a58
qixunnnn <64120978+qixunnnn@users.noreply.github.com>
2020-04-24 22:56:26 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -26,7 +26,38 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
-    
+     int input;
+     ArrayList<Integer> allInt = new ArrayList<Integer>();
+
+     System.out.print("Enter the amount of numbers: ");
+     Scanner in = new Scanner(System.in);
+     int num = in.nextInt();
+
+     for (int i=0;i<num;i++)
+     {
+         input = in.nextInt();
+         allInt.add(input);
+     }
+
+     int iCounter = 0;
+     int imostCount = 0;
+     int iMode = 0;
+     for (int j=0;j<allInt.size();j++)
+     {
+         for (int k=0;k<allInt.size();k++)
+         {
+             if (allInt.get(j) == allInt.get(k))
+             {
+                 iCounter++;
+             }
+         }
+         if(iCounter > imostCount)
+         {
+             iMode = allInt.get(j);
+             imostCount = iCounter;
+         }
+         iCounter = 0;
+     }
+     System.out.println("The mode is: " + iMode);
   }
 }
